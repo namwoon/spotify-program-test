c7fd51a143b92524bd0d06b31f62d2cadca081de
nam <namhpham01022@gmail.com>
2024-09-05 17:41:01 -0700

    finished module adding user interactivity with the program, allows user to choose which method to run

--- spotifyreq.py
+++ spotifyreq.py
@@ -62,7 +62,7 @@ def get_top_album(token, artist_id):
     url = f"https://api.spotify.com/v1/albums/{albumid}"
     headers = get_auth_header(token)
     result = get(url, headers=headers)
-    json_result = json.loads(result.content)["images"][1]["url"]
+    json_result = json.loads(result.content)
     return json_result
 
 
@@ -90,15 +90,37 @@ def imagetoascii(url):
     with open("ascii_image.txt", "w") as f:
         f.write(ascii_image)
 
+#user interactivity
 
-
+x = 10;
 token = get_token()
-result = search_for_artist(token, "Taylor Swift")
-artist_id = result["id"]
-songs = get_songs_by_artist(token, artist_id)
-
-for idx, song in enumerate(songs):
-    print(f"{idx + 1}.{song['album']['name']}")
-
-print(get_top_album(token,artist_id))
-imagetoascii(get_top_album(token, artist_id))
+artist_chosenflag = False
+while(x > 0):
+    
+    if not artist_chosenflag:
+       artist = input("Please choose an artist:")
+       result = search_for_artist(token, artist)
+       artist_chosenflag = True
+       artist_id = result["id"]
+       
+    print("\nWhat would you like to see about this artist?")
+    print("1. Top songs!")
+    print("2. Top album!")
+    print("3. Ascii!")
+    print("4. Search for new artist")
+    print("-1 Quit")
+    value = input();
+    if int(value) == 1:
+        songs = get_songs_by_artist(token, artist_id)
+        print("\n Their top songs are: ")
+        for idx, song in enumerate(songs):
+         print(f"{idx + 1}.{song['name']}")
+    if int(value) == 2:
+        print("\nTheir top album is: " + get_top_album(token,artist_id)["name"])
+    if int(value) == 3:
+        imagetoascii(get_top_album(token, artist_id)["images"][1]["url"])
+        print("\nASCII image of their top album cover has been generated!\n")
+    if int(value) == 4:
+        artist_chosenflag = False
+    if int(value) == -1:
+        x = -1
